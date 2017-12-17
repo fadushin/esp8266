a60a7999b1718aa29d23d0aaea40192924bf2e93
Fred Dushin <fred@dushin.net>
2017-12-17 12:19:57 -0500

    Improved ulog, because I am getting sick of the micropython-lib logging library.

---
+++ micropython/ulog/main.py
@@ -0,0 +1,9 @@
+import esp
+esp.osdebug(None)
+
+##
+## These make command-line hacking easier
+## remove as part of production
+##
+from core.cmd import *
+
--- micropython/ulog/ulog/__init__.py
+++ micropython/ulog/ulog/__init__.py
@@ -27,6 +27,8 @@
 import sys
 import utime
 
+import core.util
+
 class Log :
     '''
     This object encapsulates common logging operations
@@ -43,21 +45,21 @@ class Log :
         self._levels = config['levels']
         self._name = config['name']
         
-    def debug(self, format_str, args = ()) :
+    def debug(self, format_str, *args) :
         '''Send a debug log message'''
-        self.log(self.DEBUG, format_str, args)
+        self.log(self.DEBUG, format_str, *args)
 
-    def info(self, format_str, args = ()) :
+    def info(self, format_str, *args) :
         '''Send an info log message'''
-        self.log(self.INFO, format_str, args)
+        self.log(self.INFO, format_str, *args)
 
-    def warning(self, format_str, args = ()) :
+    def warning(self, format_str, *args) :
         '''Send a warning log message'''
-        self.log(self.WARNING, format_str, args)
+        self.log(self.WARNING, format_str, *args)
 
-    def error(self, format_str, args = ()) :
+    def error(self, format_str, *args) :
         '''Send an error log message'''
-        self.log(self.ERROR, format_str, args)
+        self.log(self.ERROR, format_str, *args)
     
     ##
     ## internal operations
@@ -70,7 +72,7 @@ class Log :
             for name, config in sink_configs.items() :
                 try :
                     sink_name = name + "_sink"
-                    mod = __import__(sink_name)
+                    mod = __import__(sink_name, globals(), locals(), ['Sink'], 0)
                     ret[name] = mod.Sink(config)
                     print("loaded sink {}".format(name))
                 except Exception as e :
@@ -80,17 +82,18 @@ class Log :
         else :
             return {}
             
-    def create(self, level, format_str, args) :
+    def create(self, level, format_str, *args) :
+        message = format_str.format(*args) if args != () else format_str
         return {
             'name': self._name,
             'datetime': self.datetimestr(),
             'level': level,
-            'message': format_str % args
+            'message': message
         }
     
-    def log(self, level, format_str, args = ()) :
+    def log(self, level, format_str, *args) :
         if level in self._levels :
-            message = self.create(level, format_str, args)
+            message = self.create(level, format_str, *args)
             for name, sink in self._sinks.items() :
                 self.do_log(sink, message)
     
@@ -103,60 +106,51 @@ class Log :
     def datetimestr(self) :
         import sys
         if sys.platform == 'esp8266':
-            year, month, day, hour, minute, second, millis, _tzinfo = utime.localtime()
-            return "%d-%02d-%02dT%02d:%02d:%02d.%03d" % (year, month, day, hour, minute, second, millis)
+            return core.util.secs_to_string()
         else:
             return utime.strftime("%Y-%m-%dT%H:%M:%S.0000")
 
 
-
-def module_to_dict(mod) :
-    ret = {}
-    for i in dir(mod) :
-        if not i.startswith('__') :
-            ret[i] = getattr(mod, i)
-    return ret
 
 def merge_dict(a, b) :
     ret = a.copy()
     ret.update(b)
     return ret
 
-def get_config() :
+def get_config(path="/ulog.json") :
     defaults = {
         'name': "esp8266",
         'levels': [Log.INFO, Log.WARNING, Log.ERROR],
-        'sinks': {'console': None}
+        'sinks': {'ulog.console': {}}
     }
     try :
-        import log_config
         return merge_dict(
-            defaults, module_to_dict(log_config)
+            defaults, core.util.load_json(path)
         )
     except Exception as e :
         return defaults
 
 logger = Log(get_config())
 
-def debug(format_str, args = ()) :
+def debug(format_str, *args) :
     '''Send a debug log message'''
-    logger.debug(format_str, args)
+    logger.debug(format_str, *args)
 
-def info(format_str, args = ()) :
+def info(format_str, *args) :
     '''Send an info log message'''
-    logger.info(format_str, args)
+    logger.info(format_str, *args)
 
-def warning(format_str, args = ()) :
+def warning(format_str, *args) :
     '''Send a warning log message'''
-    logger.warning(format_str, args)
+    logger.warning(format_str, *args)
 
-def error(format_str, args = ()) :
+def error(format_str, *args) :
     '''Send an error log message'''
-    logger.error(format_str, args)
+    logger.error(format_str, *args)
 
 def test(ub=10) :
     for i in range(0, ub):
-        logger.debug("%s %s", ('debug', 'world'))
-        logger.info("%s %s", ('info', 'world'))
-        logger.warning("%s %s", ('warning', 'world'))
-        logger.error("%s %s", ('error', 'world'))
+        logger.debug("{} {}", 'debug', 'world')
+        logger.info("{} {}", 'info', 'world')
+        logger.warning("{} {}", 'warning', 'world')
+        logger.error("{} {}", 'error', 'world')
